f48d532126ff7ce8eb98c6ec151652b597832a42
ZenMasterChris <mr.christopher.perry@gmail.com>
2014-09-18 10:55:39 -0700

    boom

--- app/src/androidTest/java/com/ensolabs/testingandroiddi/activity/MainActivityTest.java
+++ app/src/androidTest/java/com/ensolabs/testingandroiddi/activity/MainActivityTest.java
@@ -3,6 +3,13 @@ package com.ensolabs.testingandroiddi.activity;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.ensolabs.testingandroiddi.R;
+import com.ensolabs.testingandroiddi.injection.MainActivityInjector;
+import com.ensolabs.testingandroiddi.thing.Thing;
+
+import javax.inject.Named;
+
+import dagger.Module;
+import dagger.Provides;
 
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
@@ -14,13 +21,85 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     super(MainActivity.class);
   }
 
-  @Override protected void setUp() throws Exception {
-    super.setUp();
+  public void testTheTestThingsWereInjectedOne() {
+    // Given
+    String thingOneText = "I'm TEST ONE Thing One!";
+    String thingTwoText = "I'm TEST ONE Thing Two!";
+    TestThingModule testThingModule = new TestThingModule(
+      thing(thingOneText),
+      thing(thingTwoText)
+    );
+
+    // When
+    MainActivityInjector.setModules(testThingModule);
     getActivity();
+
+    // Then
+    onView(withId(R.id.thingOneText)).check(matches(withText(thingOneText)));
+    onView(withId(R.id.thingTwoText)).check(matches(withText(thingTwoText)));
   }
 
-  public void testTheTestThingsWereInjected() {
-    onView(withId(R.id.thingOneText)).check(matches(withText("I'm TEST Thing One!")));
-    onView(withId(R.id.thingTwoText)).check(matches(withText("I'm TEST Thing Two!")));
+  public void testTheTestThingsWereInjectedTwo() {
+    // Given
+    String thingOneText = "I'm TEST TWO Thing One!";
+    String thingTwoText = "I'm TEST TWO Thing Two!";
+    TestThingModule testThingModule = new TestThingModule(
+      thing(thingOneText),
+      thing(thingTwoText)
+    );
+
+    // When
+    MainActivityInjector.setModules(testThingModule);
+    getActivity();
+
+    // Then
+    onView(withId(R.id.thingOneText)).check(matches(withText(thingOneText)));
+    onView(withId(R.id.thingTwoText)).check(matches(withText(thingTwoText)));
+  }
+
+  Thing thing(final String speech) {
+    return new Thing() {
+      @Override public String speak() {
+        return speech;
+      }
+    };
+  }
+
+  @Module(
+    injects = MainActivity.class,
+    includes = BasicModule.class,
+    overrides = true
+  )
+  public static class TestThingModule {
+    private final Thing thingOne;
+    private final Thing thingTwo;
+
+    public TestThingModule(Thing thingOne, Thing thingTwo) {
+      this.thingOne = thingOne;
+      this.thingTwo = thingTwo;
+    }
+
+    @Provides @Named("Thing 1") public Thing thingOne() {
+      return thingOne;
+    }
+
+    @Provides @Named("Thing 2") public Thing thingTwo() {
+      return thingTwo;
+    }
+  }
+
+  @Module(
+    library = true,
+    complete = false
+  )
+  public static class BasicModule {
+
+    @Provides @Named("Thing 2") public Thing thingTwo() {
+      return new Thing() {
+        @Override public String speak() {
+          return "I AM the THING!";
+        }
+      };
+    }
   }
 }
--- app/src/testing/java/com/ensolabs/testingandroiddi/injection/MainActivityInjector.java
+++ app/src/testing/java/com/ensolabs/testingandroiddi/injection/MainActivityInjector.java
@@ -1,14 +1,18 @@
 package com.ensolabs.testingandroiddi.injection;
 
 import com.ensolabs.testingandroiddi.activity.MainActivity;
-import com.ensolabs.testingandroiddi.thing.TestThingModule;
 
 import dagger.ObjectGraph;
 
 public class MainActivityInjector {
+  private static Object[] modules = new Object[0];
 
   public static void inject(MainActivity activity) {
-    ObjectGraph objectGraph = ObjectGraph.create(new TestThingModule());
+    ObjectGraph objectGraph = ObjectGraph.create(modules);
     objectGraph.inject(activity);
+  }
+
+  public static void setModules(Object... modules) {
+    MainActivityInjector.modules = modules;
   }
 }
